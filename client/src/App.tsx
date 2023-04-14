f4cef2f4222e5ae3ea7c52d6b8ebf0ec3c2dc3b8
GabrielTeixeiraC <69649169+GabrielTeixeiraC@users.noreply.github.com>
2023-04-14 15:46:31 -0300

    Feat: Profile Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import Login from './components/Login/Login'
 import SignUp from './components/SignUp/SignUp';
 import Home from './components/Home/Home';
+import Profile from './components/Profile/Profile';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 
@@ -14,11 +15,8 @@ function App() {
             element={<Home />}
           />
           <Route
-            path="/profile"
-            element={
-              <>
-              </>
-            }
+            path="/me"
+            element={<Profile />}
           />
           <Route path="/signup" element={<SignUp />} />
           <Route index element={<Login />} />
